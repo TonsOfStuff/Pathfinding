1506261a06d55108d841b588825c5456c2c5f6f8
Jerry Du <jtons2008@gmail.com>
2026-09-08 18:31:32 -0500

    Fixed rain down error for DFS and added an order list return which works better than a set

--- algs.py
+++ algs.py
@@ -3,28 +3,34 @@ def bfs(start, end):
     queue = [start]
     visited = {start}
     path = {}
+    order = [start]
     while queue:
         current = queue.pop(0)
         if current == end:
-            return path
+            return path, order
         for neighbor in current.neighbors:
             if neighbor not in visited:
                 visited.add(neighbor)
                 queue.append(neighbor)
                 path[neighbor] = current
-    return visited
+                order.append(neighbor)
+    return None, order
 
 def dfs(start, end):
     stack = [start]
     visited = {start}
     path = {}
+    order = [start]          
+    if start == end:
+        return path, order
     while stack:
-        current = stack.pop();
-        if (current == end):
-            return path
+        current = stack.pop()
         for neighbor in current.neighbors:
             if neighbor not in visited:
                 visited.add(neighbor)
-                stack.append(neighbor)
                 path[neighbor] = current
-    return visited
+                order.append(neighbor)     
+                if neighbor == end:
+                    return path, order
+                stack.append(neighbor)
+    return None, order
